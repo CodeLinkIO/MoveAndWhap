c9e36a2d8f205c2e37406dad67c2839422d2b738
Kai Six <petersen.codie@gmail.com>
2022-12-12 17:47:46 -0800

    Example inline documentation.

--- examples/contractWithEthersService.mjs
+++ examples/contractWithEthersService.mjs
@@ -17,32 +17,50 @@ const mawAbi = [
     "event PlayerMoved(address indexed player, int256 indexed x, int256 indexed y, int8 dir)",
     "event PlayerAttacked(address indexed attacker, address indexed victim)",
 ];
+
+//When you rpovide a private key, the EtherService creates a signer. If you want a signer, make sure to
+//proivde a private key or use a signer from the web user.
 const mawContract = new ethers.Contract(process.env.MAW_CONTRACT_ADDRESS, mawAbi, service.signer);
 const preKill = true;
 const postKill = false;
 
 //Kill the player if they were not killed last time so we can join.
 if(preKill) {
-    await new Promise(r => setTimeout(r, timeout));
-    let response = await mawContract.whap(process.env.PRIVATE_KEY_ADDRESS); 
-    await response.wait().then(x => console.log(x));   
+    try {
+        await new Promise(r => setTimeout(r, timeout));
+        let response = await mawContract.whap(process.env.PRIVATE_KEY_ADDRESS); 
+        await response.wait().then(x => console.log(x));   
+    } catch(error) {
+        console.Error(error);
+    }
 }
 
 //Join
-let response = await mawContract.join(123,45,0);
-await response.wait().then(x => console.log(x));
-
+try {
+    let response = await mawContract.join(123,45,0);
+    await response.wait().then(x => console.log(x));
+} catch(error) {
+    console.error(error);
+}
 //Make random moves.
 for(let m = 0; m < movesToMake; m++) {
-    await new Promise(r => setTimeout(r, timeout));            //2 second wait time.
-    let randomDirection = Math.floor(Math.random()*4);     //Get a random direction.
-    let response = await mawContract.move(randomDirection); //Do the actual move.
-    await response.wait().then(x => console.log(x));        //Wait and log the transaction.
+    try {
+        await new Promise(r => setTimeout(r, timeout));         //2 second wait time.
+        let randomDirection = Math.floor(Math.random()*4);      //Get a random direction.
+        let response = await mawContract.move(randomDirection); //Do the actual move.
+        await response.wait().then(x => console.log(x));        //Wait and log the transaction.
+    } catch(error) {
+        console.error(error);
+    }
 }
 
 //Kill yourself so if you run this script again it won't crash because of double joining.
 if(postKill) {
-    await new Promise(r => setTimeout(r, timeout));
-    let response = await mawContract.whap(process.env.PRIVATE_KEY_ADDRESS); 
-    await response.wait().then(x => console.log(x));   
+    try {
+        await new Promise(r => setTimeout(r, timeout));
+        let response = await mawContract.whap(process.env.PRIVATE_KEY_ADDRESS); 
+        await response.wait().then(x => console.log(x));   
+    } catch(error) {
+        console.error(error);
+    }
 }

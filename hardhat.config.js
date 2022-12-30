a0fba0a86ab75f42ca7cdc0d8df540a0533c0725
Codie Petersen <codiepetersen@Codies-Mac-mini.local>
2022-12-29 21:19:12 -0800

    Added subnet connection ability.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,6 @@
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const fujiURL = process.env.PROVIDER_URL;
-const fujiKey = process.env.PRIVATE_KEY;
-
 module.exports = {
   solidity: "0.8.17",
   paths:{
@@ -13,8 +10,12 @@ module.exports = {
   },
   networks: {
     fuji: {
-      url: fujiURL,
-      accounts: [fujiKey]
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [ "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" ]
+    },
+    local_subnet: {
+      url: "http://127.0.0.1:9656/ext/bc/HSF3s7g5wwJ2ipd7ACsu1yAvf9V63ZWU2pmT223nWTizseytS/rpc",
+      accounts: [ "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" ]
     }
   }
 };
